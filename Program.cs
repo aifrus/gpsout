58041ad615df7b2862075d7a302832d6feeb2483
Russell Purinton <russell.purinton@gmail.com>
2024-01-31 11:54:22 -0500

    x

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.FlightSimulator.SimConnect;
 
-namespace gpsout
+namespace SimConnectApp
 {
-    internal class Program
+    class Program
     {
+        private static SimConnect SimConnect = null;
+
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Attempting to connect to SimConnect...");
+                SimConnect = new SimConnect("Managed Data Request", IntPtr.Zero, 0, null, 0);
+                SimConnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(SimConnect_OnRecvOpen);
+                SimConnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(SimConnect_OnRecvQuit);
+                Console.WriteLine("SimConnect initialized. Waiting for connection...");
+
+                // Loop until SimConnect is null, which happens when the OnRecvQuit event is triggered.
+                while (SimConnect != null)
+                {
+                    System.Threading.Thread.Sleep(1000); // Sleep for 1 second.
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to connect to SimConnect: " + ex.Message);
+            }
+        }
+
+        static void SimConnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+            Console.WriteLine("Connected to SimConnect.");
+        }
+
+        static void SimConnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            Console.WriteLine("SimConnect connection closed.");
+            SimConnect = null;
         }
     }
 }
