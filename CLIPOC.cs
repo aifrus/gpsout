8d514de240b1fe6acbb5f0be6ff48eabdb26ab04
Russell Purinton <russell.purinton@gmail.com>
2024-02-01 09:25:32 -0500

    x

---
+++ CLIPOC.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Aifrus.GPSout
+{
+    class CLIPOC
+    {
+        private static readonly SerialPort SerialPort = new SerialPort("COM1");
+        static void Main(string[] args)
+        {
+            SerialPort.Open();
+            using (var simConnectClient = new SimConnectClient())
+            {
+                simConnectClient.OnDataReceived += SimConnectClient_OnDataReceived;
+
+                // Start a new thread for the SimConnect data request
+                var simConnectThread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        simConnectClient.RequestData();
+                        Thread.Sleep(1000); // Wait for 1 second before sending the next data request
+                    }
+                });
+
+                simConnectThread.Start();
+
+                // Wait for the Escape key to be pressed
+                while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Escape)
+                {
+                    // Keep the main thread running
+                }
+
+                // Stop the SimConnect thread when the Escape key is pressed
+                simConnectThread.Abort();
+            }
+            SerialPort.Close();
+        }
+
+
+        private static void SimConnectClient_OnDataReceived(double latitude, double longitude)
+        {
+            Console.WriteLine($"Data received: Latitude = {latitude}, Longitude = {longitude}");
+
+            string formattedLatitude = FormatCoordinate(latitude);
+            string formattedLongitude = FormatCoordinate(longitude);
+
+            NMEAEncoder encoder = new NMEAEncoder
+            {
+                UTCTime = DateTime.UtcNow.ToString("HHmmss"),
+                Latitude = formattedLatitude,
+                Longitude = formattedLongitude,
+                Altitude = "545.4", // You may want to replace this with actual data
+                Course = "075.4", // You may want to replace this with actual data
+                Speed = "022.4" // You may want to replace this with actual data
+            };
+
+            string gpggaSentence = encoder.EncodeGPGGA();
+            string gpgllSentence = encoder.EncodeGPGLL();
+            string gprmcSentence = encoder.EncodeGPRMC();
+            string gpvtgSentence = encoder.EncodeGPVTG();
+
+            SerialPort.WriteLine(gpggaSentence);
+            SerialPort.WriteLine(gpgllSentence);
+            SerialPort.WriteLine(gprmcSentence);
+            SerialPort.WriteLine(gpvtgSentence);
+        }
+
+        private static string FormatCoordinate(double coordinate)
+        {
+            int degrees = (int)coordinate;
+            double decimalMinutes = (coordinate - degrees) * 60;
+            return $"{degrees.ToString("00")}{decimalMinutes.ToString("00.000")}";
+        }
+    }
+}
---
+++ SimConnectClient.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Aifrus.GPSout
+{
+    internal class SimConnectClient : IDisposable
+    {
+        private SimConnect simConnect = null;
+        private const int WM_USER_SIMCONNECT = 0x0402;
+
+        enum DEFINITIONS
+        {
+            Struct1,
+        }
+#pragma warning disable 0649
+        struct Struct1
+        {
+            public double Latitude;
+            public double Longitude;
+        }
+#pragma warning restore 0649
+
+
+        public event Action<double, double> OnDataReceived;
+        public SimConnectClient()
+        {
+            try
+            {
+                simConnect = new SimConnect("Managed Data Request", IntPtr.Zero, WM_USER_SIMCONNECT, null, 0);
+                simConnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Latitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simConnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Longitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simConnect.RegisterDataDefineStruct<Struct1>(DEFINITIONS.Struct1);
+                simConnect.OnRecvSimobjectDataBytype += SimConnect_OnRecvSimobjectDataBytype;
+                Console.WriteLine("Connected to Flight Simulator");
+            }
+            catch (COMException ex)
+            {
+                Console.WriteLine("Unable to connect to Flight Simulator " + ex.Message);
+            }
+        }
+
+        private void SimConnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+            Console.WriteLine("OnRecvSimobjectDataBytype event triggered");
+
+            if (data.dwRequestID == (uint)DEFINITIONS.Struct1)
+            {
+                Struct1 s1 = (Struct1)data.dwData[0];
+                OnDataReceived?.Invoke(s1.Latitude, s1.Longitude);
+            }
+        }
+
+        public void RequestData()
+        {
+            simConnect.RequestDataOnSimObjectType(DEFINITIONS.Struct1, DEFINITIONS.Struct1, 1000000, SIMCONNECT_SIMOBJECT_TYPE.USER);
+        }
+
+
+        public void Dispose()
+        {
+            simConnect?.Dispose();
+        }
+    }
+}
